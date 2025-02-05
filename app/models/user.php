08a5b7343294561eeb7ad9f5ccdb67341e9acae8
lakrounehamza <lakrounehamza10@gmail.com>
2025-02-05 14:22:14 +0100

    ajoute l' encapsulation de class User

--- app/models/user.php
+++ app/models/user.php
@@ -1,74 +1,63 @@
 <?php
-
-// require_once './../Model/User.php';
-// require_once '../../Core/Database.php';
-
-class User {
-    private $id;  
+class User
+{
+    private $id;
     private $nom;
     private $prenom;
     private $email;
-    private $mot_de_passe;
+    private $password;
     private $role;
     private $date_creation;
 
-    public function __construct() {
-       
+    public function  __construct() {}
 
+    public function setId($id)
+    {
+        if (is_int($id) && $id > 0)
+            $this->id = $id;
     }
-
-    public function getNom() {
-        return $this->nom;
+    public function  setNom($nom)
+    {
+        if (preg_match('/^[a-zA-Z\s]{3,50}$/', $nom))
+            $this->nom = $nom;
     }
-
-    public function setNom($nom) {
-        $this->nom = $nom;
+    public function  setPrenom($prenom)
+    {
+        if (preg_match('/^[a-zA-Z\s]{3,50}$/', $prenom))
+            $this->prenom = $prenom;
     }
-
-    public function getPrenom() {
-        return $this->prenom;
+    public function  setEmail($email)
+    {
+        if (preg_match('/^[a-zA-Z0-9]{4,50}@gmail.com$/', $email))
+            $this->email = $email;
     }
-
-    public function setPrenom($prenom) {
-        $this->prenom = $prenom;
+    public function  setDate_creation($date){
+        $this->date_creation = $date;
     }
-
-    public function getEmail() {
-        return $this->email;
+    public function  setPassword($password)
+    {
+        if (strlen($password) >= 8)
+            $this->password = $password;
     }
-
-    public function setEmail($email) {
-        $this->email = $email;
-    }
-
-    public function getMotDePasse() {
-        return $this->mot_de_passe;
-    }
-
-    public function setMotDePasse($mot_de_passe) {
-        $this->mot_de_passe = $mot_de_passe;
-    }
-
-    public function getRole() {
-        return $this->role;
-    }
-
-    public function setRole($role) {
-        $this->role = $role;
-    }
-
-    public function getDateCreation() {
-        return $this->date_creation;
-    }
-
-    public function setDateCreation($date_creation) {
-        $this->date_creation = $date_creation;
-    }
-    public function getId() {
+    public function  getId(){
         return $this->id;
     }
-
-    public function setId($id) {
-        $this->id = $id;
+    public function  getNom(){
+        return $this->nom;
+    }
+    public function  getPrenom(){
+        return $this->prenom;
+    }
+    public function  getEmail(){
+        return $this->email;
+    }
+    public function  getPassword(){
+        return  $this->password ;
+    }
+    public function  getRole(){
+        return  $this->role;
+    }
+    public function  getDate_creation(){
+        return $this->date_creation;
     }
 }
